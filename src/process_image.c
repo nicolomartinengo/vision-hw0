d01e8709fc26db1e0f33c62c1f904db67f391312
Nicolò Martinengo <157996290+nicolomartinengo@users.noreply.github.com>
2024-12-16 17:36:50 +0100

    Update process_image.c

--- src/process_image.c
+++ src/process_image.c
@@ -6,14 +6,39 @@
 
 float get_pixel(image im, int x, int y, int c)
 {
-    // TODO Fill this in
-    return 0;
+    // Clamp x to be within [0, im.w - 1]
+    if (x < 0) x = 0;
+    if (x >= im.w) x = im.w - 1;
+
+    // Clamp y to be within [0, im.h - 1]
+    if (y < 0) y = 0;
+    if (y >= im.h) y = im.h - 1;
+
+    // Clamp c to be within [0, im.c - 1]
+    if (c < 0) c = 0;
+    if (c >= im.c) c = im.c - 1;
+
+    // Calculate 1D index for the pixel value
+    int index = c * im.w * im.h + y * im.w + x;
+
+    // Return the pixel value
+    return im.data[index];
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
-    // TODO Fill this in
+    // Ignore if x, y, or c is out of bounds
+    if (x < 0 || x >= im.w || y < 0 || y >= im.h || c < 0 || c >= im.c) {
+        return;
+    }
+
+    // Calculate 1D index for the pixel value
+    int index = c * im.w * im.h + y * im.w + x;
+
+    // Set the pixel value
+    im.data[index] = v;
 }
+
 
 image copy_image(image im)
 {
